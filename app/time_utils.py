dd1d4882fc5baa4b5e2f5d55c9429eeef0a48373
expertmedia-svg <expertmedia@comstratmedia.com>
2026-03-25 19:56:51 +0000

    p

--- app/time_utils.py
+++ app/time_utils.py
@@ -1,9 +1,7 @@
-from datetime import UTC, datetime
+from datetime import datetime, timezone
 
-
-def utc_now() -> datetime:
-    """Return a naive UTC datetime without relying on deprecated utcnow()."""
-    return datetime.now(UTC).replace(tzinfo=None)
+def utc_now():
+    return datetime.now(timezone.utc)
 
 
 def utc_now_iso() -> str:
